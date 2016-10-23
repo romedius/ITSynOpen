446eac5a8ec59feac213551e56e300fc743c4ba4
Romedius Weiss <romedius.weiss@gmail.com>
2016-10-23 20:24:03 +0200

    changed api level to 1.3

--- server/status.php
+++ server/status.php
@@ -1,33 +1,38 @@
 <?php
-	header('Content-type: application/json');
-	$str=file_get_contents('status.txt');
-        $arr=explode(',',$str);
-        $open=$arr[0];
-        $date=$arr[1]; //lastchange: upadate via file see explode/implode
-        echo <<< eot
+    header('Content-type: application/json');
+    $str=file_get_contents('status.txt');
+    $arr=explode(',',$str);
+    $open=$arr[0];
+    $date=$arr[1]; //lastchange: upadate via file see explode/implode
+    echo <<< eot
 {
-  "api":"0.12",
+  "api":"0.13",
   "space":"IT-Syndikat",
+  "logo":"http://it-syndikat.org/api/images/its_l.png",
   "url":"http://it-syndikat.org",
-  "icon":{
-    "open":"http://it-syndikat.org/images/ITS_Open.png",
-    "closed":"http://it-syndikat.org/images/ITS_Closed.png"
+  "location":{
+    "address":"Tschamlerstrasse 3, 6020 Innsbruck, Austria",
+    "lat":47.2578,
+    "lon":11.3961
   },
-  "address":"Tschamlerstrasse 3, 6020 Innsbruck, Austria",
+  "state":{
+    "open":$open,
+    "lastchange":$date,
+    "icon":{
+      "open":"http://it-syndikat.org/api/images/ITS_Open.png",
+      "closed":"http://it-syndikat.org/api/images/ITS_Closed.png"
+    }
+  },
   "contact":{
     "phone":"+43512563468",
     "twitter":"@ItSyndikat",
     "email":"wir@it-syndikat.org",
     "ml":"its-public@lists.catbull.com"
   },
-  "open":$open,
-  "lastchange":$date,
-  "logo":"http://it-syndikat.org/images/its_l.png",
-  "lat":47.2578,
-  "lon":11.3961
+  "issue_report_channels":["email"],
+  "ext_ccc": "chaostreff"
 }
 
 eot;
 
 ?>
-
